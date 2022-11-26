b6b03ea9557872e1e261bc460d83bdcab9b9e896
Erich-Reitz <erreitz@outlook.com>
2022-11-25 21:37:08 -0600

    solve 12pt2

--- 2021/12/twelve/src/Main.scala
+++ 2021/12/twelve/src/Main.scala
@@ -4,6 +4,7 @@ import scala.collection.mutable.ArrayBuffer
 
 
 
+//noinspection DuplicatedCode
 object Main extends App {
   def readCaves(): mutable.HashMap[String, ArrayBuffer[String]] = {
     val caves: mutable.HashMap[String, ArrayBuffer[String]] = mutable.HashMap()
@@ -20,7 +21,7 @@ object Main extends App {
 
   def part1(): Unit = {
     val caves = readCaves()
-    var completedPaths = ArrayBuffer[ArrayBuffer[String]]()
+    val completedPaths = ArrayBuffer[ArrayBuffer[String]]()
 
     def traverse(currentCaveName: String, visited: mutable.Set[String], currentPath: ArrayBuffer[String]): Unit = {
       if (currentCaveName == "end") {
@@ -45,6 +46,51 @@ object Main extends App {
     println(completedPaths.length)
   }
 
-  part1()
+  def part2(): Unit = {
+    val caves = readCaves()
+    val completedPaths = ArrayBuffer[ArrayBuffer[String]]()
+    def canMove(visited: mutable.HashMap[String, Int], name: String, hasUtilizedException: Boolean): Int = {
+      val smallCave = Character.isLowerCase(name(0))
+      if (!smallCave) {
+        return 1
+      }
+
+      if (visited.getOrElse(name, 0) == 0) {
+        return 1
+      }
+
+      if (!hasUtilizedException && (name != "start" && name != "end")) {
+        return 2
+      }
+
+      0
+    }
+
+
+    def traverse(currentCaveName: String, visited: mutable.HashMap[String, Int], currentPath: ArrayBuffer[String], hasUtilizedException: Boolean): Unit = {
+      if (currentCaveName == "end") {
+        currentPath += currentCaveName
+        completedPaths += currentPath
+        return
+      }
+      visited(currentCaveName) = visited.getOrElse(currentCaveName, 0) + 1
+      currentPath += currentCaveName
+      val nextCaves = caves.getOrElse(currentCaveName, ArrayBuffer[String]())
+      for (cave <- nextCaves) {
+        val moveStatus = canMove(visited, cave, hasUtilizedException)
+        if (moveStatus >= 1) {
+          if (moveStatus == 2) traverse(cave, visited, currentPath.clone(), true)
+          else traverse(cave, visited, currentPath.clone(), hasUtilizedException)
+        }
+      }
+      visited(currentCaveName) = visited.getOrElse(currentCaveName, 0) - 1
+      currentPath.remove(currentPath.length - 1)
+    }
+
+    traverse("start", mutable.HashMap[String, Int](), ArrayBuffer[String](), false)
+    println(completedPaths.length)
+  }
+
+part2()
 
 }
