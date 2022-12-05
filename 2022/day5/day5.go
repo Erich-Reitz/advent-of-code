d48639db66d127220f1cdcbefdcc5cc1f6623ba4
Erich-Reitz <erreitz@outlook.com>
2022-12-05 00:02:11 -0600

    go fmt

--- 2022/day5/day5.go
+++ 2022/day5/day5.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/Erich-Reitz/commonGo"
+	"reflect"
 	"regexp"
 	"strings"
-	"reflect"
 )
 
 func check(e error) {
@@ -41,34 +41,33 @@ func getMoveInstructions(lines []string) []string {
 }
 
 func ReverseSlice(s []string) {
-    size := reflect.ValueOf(s).Len()
-    swap := reflect.Swapper(s)
-    for i, j := 0, size-1; i < j; i, j = i+1, j-1 {
-        swap(i, j)
-    }
+	size := reflect.ValueOf(s).Len()
+	swap := reflect.Swapper(s)
+	for i, j := 0, size-1; i < j; i, j = i+1, j-1 {
+		swap(i, j)
+	}
 }
 
 func part1(cargo map[int][]string, moveInstructions []string) {
 	for _, line := range moveInstructions {
-		var amount, indexFrom, indexTo int 
-		_, err := fmt.Sscanf(line, "move %d from %d to %d", &amount, &indexFrom, &indexTo) 
+		var amount, indexFrom, indexTo int
+		_, err := fmt.Sscanf(line, "move %d from %d to %d", &amount, &indexFrom, &indexTo)
 		check(err)
 		toMove := cargo[indexFrom][len(cargo[indexFrom])-amount:]
 		cargo[indexFrom] = cargo[indexFrom][:len(cargo[indexFrom])-amount]
-		cargo[indexTo] = append(cargo[indexTo], toMove...) 
+		cargo[indexTo] = append(cargo[indexTo], toMove...)
 	}
 	printLastEntryOfEachCargoStack(cargo)
 }
 
 func printLastEntryOfEachCargoStack(cargo map[int][]string) {
 	for i := 1; i <= 9; i++ {
-		values := cargo[i] 
+		values := cargo[i]
 		if len(values) != 0 {
 			fmt.Print(cargo[i][len(cargo[i])-1])
 		}
 	}
 }
-
 
 func getInitialCargoState(lines []string) map[int][]string {
 	cargo := make(map[int][]string)
@@ -81,19 +80,19 @@ func getInitialCargoState(lines []string) map[int][]string {
 				cargo[index] = append([]string{value}, cargo[index]...)
 			}
 		}
-	}	
+	}
 	return cargo
 }
 
 func part2(cargo map[int][]string, moveInstructions []string) {
 	for _, line := range moveInstructions {
-		var amount, indexFrom, indexTo int 
-		_, err := fmt.Sscanf(line, "move %d from %d to %d", &amount, &indexFrom, &indexTo) 
+		var amount, indexFrom, indexTo int
+		_, err := fmt.Sscanf(line, "move %d from %d to %d", &amount, &indexFrom, &indexTo)
 		check(err)
 		toMove := cargo[indexFrom][len(cargo[indexFrom])-amount:]
 		cargo[indexFrom] = cargo[indexFrom][:len(cargo[indexFrom])-amount]
 		ReverseSlice(toMove)
-		cargo[indexTo] = append(cargo[indexTo], toMove...) 
+		cargo[indexTo] = append(cargo[indexTo], toMove...)
 	}
 	printLastEntryOfEachCargoStack(cargo)
 }
